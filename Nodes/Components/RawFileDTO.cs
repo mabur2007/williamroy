b1db34c20d93bfc5dd2585f827deb4cd2b8d7ed2
William Roy <mabur2007@hotmail.co.uk>
2023-05-12 06:23:57 +0100

    Test

---
+++ Nodes/Components/RawFileDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Nodes.Components
+{
+    public class RawFileDTO
+    {
+        public string column1 { get; set; }
+        public string column2 { get; set; }
+        public string column3 { get; set; }
+        public string column4 { get; set; }
+    }
+}
