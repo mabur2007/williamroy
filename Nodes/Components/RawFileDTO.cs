e079b0b13ae82d3d5136890319f1464d65f7c53b
William Roy <william.roy@flagship-group.co.uk>
2023-05-16 06:10:18 +0100

    Test

--- Nodes/Components/RawFileDTO.cs
+++ Nodes/Components/RawFileDTO.cs
@@ -7,4 +7,28 @@
         public string column3 { get; set; }
         public string column4 { get; set; }
     }
+
+    public class Column1
+    {
+        public string value { get; set; }
+
+    }
+
+    public class Column2
+    {
+        public string value { get; set; }
+
+    }
+
+    public class Column3
+    {
+        public string value { get; set; }
+
+    }
+
+    public class Column4
+    {
+        public string value { get; set; }
+
+    }
 }
