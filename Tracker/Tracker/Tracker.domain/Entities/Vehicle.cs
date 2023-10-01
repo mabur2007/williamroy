0b3115ceba425c760b0624e5ee94821daddfb625
William Roy <mabur2007@hotmail.co.uk>
2023-10-01 20:06:12 +0100

    x

--- Tracker/Tracker/Tracker.domain/Entities/Vehicle.cs
+++ Tracker/Tracker/Tracker.domain/Entities/Vehicle.cs
@@ -5,6 +5,7 @@ namespace Tracker.domain.Entities
     public class Vehicle
     {
         public int Id { get; set; }
+        public Address? Address { get; set; }
         public string? RegNo { get; set; }
         public string? Color { get; set; }
         public string? Make { get; set; }
