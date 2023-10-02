eb37ec2eeff4e41846d7a5203a1c91cc59b13f27
William Roy <mabur2007@hotmail.co.uk>
2023-10-02 20:59:21 +0100

    x

--- Tracker/Tracker/Tracker.UI.App/Data/VehicleService.cs
+++ Tracker/Tracker/Tracker.UI.App/Data/VehicleService.cs
@@ -1,6 +1,10 @@
-﻿namespace Tracker.UI.App.Data
-{ 
-        public class VehicleService
+﻿using ExifLib;
+using Xamarin.Forms.Maps;
+using Geocoding;
+
+namespace Tracker.UI.App.Data
+{
+    public class VehicleService
         {
             public async Task<VehicleDTO[]> GetVehiclesAsync()
             {
@@ -9,6 +13,35 @@
          
             return response;
             }
+
+        public async Task<List<string>> GetAddressesFromImage(string Path)
+        {
+            double latitude, longitude;
+            //using (var reader = new ExifReader(Path))
+            //{
+            // Get the GPS coordinates from the EXIF data.
+
+            //    reader.GetTagValue(ExifTags.GPSLatitude, out latitude);
+            //    reader.GetTagValue(ExifTags.GPSLongitude, out longitude);
+            // }
+
+            var reader = new ExifReader(Path);
+            reader.GetTagValue(ExifTags.GPSLatitude, out latitude);
+            reader.GetTagValue(ExifTags.GPSLongitude, out longitude);
+
+            var geocoder = new Geocoder();
+            var position = new Position(latitude, longitude);
+            var address = await geocoder.GetAddressesForPositionAsync(position);
+
+            foreach (var s in address)
+            {
+                Console.WriteLine(s);
+            }
+            return address.ToList();
+
         }
+    }
+
+
 
 }
---
+++ Tracker/Tracker/Tracker.domain/Helpers/GetImagePostcode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExifLib;
+using Geocoding;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Maps;
+
+namespace Tracker.domain.Helpers
+{
+    public class GetImagePostcode
+    {
+
+        public async Task<List<string>> GetAddressesFromImage(string Path)
+        {
+            double latitude, longitude;
+            using (var reader = new ExifReader(Path))
+            {
+                // Get the GPS coordinates from the EXIF data.
+
+                reader.GetTagValue(ExifTags.GPSLatitude, out latitude);
+                reader.GetTagValue(ExifTags.GPSLongitude, out longitude);
+            }
+
+            var geocoder = new Geocoder();
+            var position = new Position(latitude, longitude);
+            var address = await geocoder.GetAddressesForPositionAsync(position);
+
+            foreach (var s in address)
+            {
+                Console.WriteLine(s);
+            }
+
+            return address.ToList();
+          
+        }
+    }
+}
